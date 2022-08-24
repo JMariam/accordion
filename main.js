bd4e55359ac81f009652558da93321a0bba472eb
Emjay <107817196+JMariam@users.noreply.github.com>
2022-08-24 01:18:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//VARIABLES
+
+const accordion = document.getElementsByClassName('accordion-content');
+
+for(i=0; i<accordion.length; i++){
+				
+				accordion[i].addEventListener('click', function () {
+								this.classList.toggle('active')
+				})
+				
+}
